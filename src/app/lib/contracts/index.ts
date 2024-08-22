0cfc4876e13cdc7af64a3fbe957d9f6f5b85ee7e
NanezX <victor.nanezj@gmail.com>
2024-08-22 19:19:16 -0400

    new addresses

--- src/app/lib/contracts/index.ts
+++ src/app/lib/contracts/index.ts
@@ -1,5 +1,5 @@
 export const addresses = {
-  SeabrickNFT: "0xfbcc62d81b2bc391a6f13e490a883dc01f689c94",
-  SeabrickMarket: "0x451abe5d0ee926e1bf48f98c700da68c6e21e55f",
+  SeabrickNFT: "0x0787b2e78a60703f6eae2fdd3b5c671369127e4e",
+  SeabrickMarket: "0x17ee4c90c6cfb6eea8bbdcd0fc5c7dc65569f12b",
   ERC20: "0x92D05C45AC5B44ae6ac5deF0bB231bC8FBdCf43a",
 };
