d03258eab4656d7f4993822a0d9cfe67b07a00ae
NanezX <victor.nanezj@gmail.com>
2024-08-22 16:15:17 -0400

    added mock erc20

--- src/app/lib/contracts/index.ts
+++ src/app/lib/contracts/index.ts
@@ -1,4 +1,5 @@
-const addresses = {
+export const addresses = {
   SeabrickNFT: "0x727fd71093ca34330a8d7a83e9c98e17dc1d4433",
   SeabrickMarket: "0x451abe5d0ee926e1bf48f98c700da68c6e21e55f",
+  ERC20: "0x92D05C45AC5B44ae6ac5deF0bB231bC8FBdCf43a",
 };
