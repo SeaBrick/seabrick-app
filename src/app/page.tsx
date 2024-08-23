e5da5f04d318aaf2371a9d3397ade268e5e3c0df
NanezX <victor.nanezj@gmail.com>
2024-08-23 06:07:44 -0400

    handled not connected wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,39 @@
+"use client";
 import ERC20Mock from "./components/contracts/erc20";
 import Seabrick from "./components/contracts/seabrick";
 import TabList from "./components/tabs/Tablist";
 import TabItem from "./components/tabs/TabItem";
 import Market from "./components/contracts/market";
+import { useAccount } from "wagmi";
 
 function App() {
+  const { isConnected } = useAccount();
+
   return (
     <div className="w-3/4 mx-auto">
-      <div className="mb-8 mt-14 text-2xl text-gray-800">Contracts</div>
+      {isConnected ? (
+        <>
+          <div className="mb-8 mt-14 text-2xl text-gray-800">Contracts</div>
 
-      <div className="">
-        <TabList>
-          <TabItem label="Market">
-            <Market />
-          </TabItem>
-          <TabItem label="Seabrick">
-            <Seabrick />
-          </TabItem>
-          <TabItem label="ERC20 Mock">
-            <ERC20Mock />
-          </TabItem>
-        </TabList>
-      </div>
+          <div className="">
+            <TabList>
+              <TabItem label="Market">
+                <Market />
+              </TabItem>
+              <TabItem label="Seabrick">
+                <Seabrick />
+              </TabItem>
+              <TabItem label="ERC20 Mock">
+                <ERC20Mock />
+              </TabItem>
+            </TabList>
+          </div>
+        </>
+      ) : (
+        <div className="mb-8 mt-14 text-2xl text-gray-800">
+          Please, connect your wallet
+        </div>
+      )}
     </div>
   );
 }
