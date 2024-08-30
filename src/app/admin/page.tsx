a5c4713f31416c028237d14211bf3d1f5bce5487
NanezX <victor.nanezj@gmail.com>
2024-08-29 21:17:29 -0400

    admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>Welcome to the admin section.</p>
+    </div>
+  );
+}
