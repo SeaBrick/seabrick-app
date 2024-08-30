6e685473d28b2657253d83de0c0079619860ed85
NanezX <victor.nanezj@gmail.com>
2024-08-29 20:32:42 -0400

    added seabrick svg

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,11 +1,12 @@
 import ConnectButton from "../buttons/ConnectButton";
+import SeabrickSVG from "./SeabrickSVG";
 
 export function Navbar() {
   return (
     <>
-      <div className="top sticky z-10 h-20 w-full min-w-[100vw] bg-white px-14 shadow">
+      <div className="top sticky z-10 h-24 w-full min-w-[100vw] bg-white px-36">
         <div className="flex h-full items-center justify-between">
-          <h1 className="text-black font-bold text-xl">Seabrick App</h1>
+          <SeabrickSVG />
 
           <div>
             <ConnectButton />
---
+++ src/app/components/layout/SeabrickSVG.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+function SeabrickSVG() {
+  return <Image src="/seabrick.svg" width={250} height={45} alt="Seabrick" />;
+}
+
+export default SeabrickSVG;
