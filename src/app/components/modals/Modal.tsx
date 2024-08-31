c0370acd856e743653a03ddd0a6171496a5c71d8
NanezX <victor.nanezj@gmail.com>
2024-08-31 17:33:21 -0400

    Modal fixed

--- src/app/components/modals/Modal.tsx
+++ src/app/components/modals/Modal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactNode } from "react";
+import { Dispatch, Fragment, ReactNode, SetStateAction } from "react";
 import {
   Dialog,
   DialogPanel,
@@ -6,62 +6,59 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 
-interface BaseModalProps {
+interface ModalProps {
   open: boolean;
-  setOpen: (value: boolean, index?: number) => void;
+  setOpen: Dispatch<SetStateAction<boolean>>;
   children: ReactNode;
 }
-const BaseModal = ({ open, setOpen, children }: BaseModalProps) => {
+const Modal = ({ open, setOpen, children }: ModalProps) => {
   const handleModalClose = (e: any) => {
     if (e.target === e.currentTarget) {
       setOpen(false);
     }
   };
   return (
-    <>
-      <Transition show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10 w-0"
-          onClose={() => setOpen(false)}
-        >
-          <div className=" min-h-screen w-0 items-center justify-center text-center">
-            <TransitionChild
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <DialogPanel className="fixed inset-0 bg-black/25" />
-            </TransitionChild>
-
-            <div className="fixed inset-0 w-screen">
-              <div className="flex min-h-full items-center justify-center text-center">
-                <TransitionChild
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                  enterTo="opacity-100 translate-y-0 sm:scale-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    <Transition show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10 w-0"
+        onClose={() => setOpen(false)}
+      >
+        <div className="text-center justify-center items-center w-0 h-0">
+          <TransitionChild
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <DialogPanel className="fixed inset-0 bg-black/25" />
+          </TransitionChild>
+          <div className="fixed inset-0 w-screen">
+            <div className="flex min-h-full items-center justify-center text-center">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <DialogPanel
+                  className="z-20"
+                  onClick={(e) => handleModalClose(e)}
                 >
-                  <Dialog.Panel
-                    className="z-20"
-                    onClick={(e) => handleModalClose(e)}
-                  >
-                    {children}
-                  </Dialog.Panel>
-                </TransitionChild>
-              </div>
+                  {children}
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
-        </Dialog>
-      </Transition>
-    </>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 
-export default BaseModal;
+export default Modal;
