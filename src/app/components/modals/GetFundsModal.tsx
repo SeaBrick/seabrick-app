9f5d2d7e371359a3060842628f046ecb03790d24
NanezX <victor.nanezj@gmail.com>
2024-08-31 18:59:58 -0400

    get token working

--- src/app/components/modals/GetFundsModal.tsx
+++ src/app/components/modals/GetFundsModal.tsx
@@ -2,19 +2,85 @@ import { Dispatch, ReactNode, SetStateAction } from "react";
 import Container from "../utils/Container";
 import Modal from "./Modal";
 import { Token } from "@/app/lib/interfaces";
+import IERC20 from "@/app/lib/contracts/abis/IERC20.json";
+import { useAccount, useWriteContract } from "wagmi";
+import { getAddress, parseUnits } from "viem";
+import { addressResumer } from "@/app/lib/utils";
 
 interface ModalProps {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
   token: Token;
 }
-export default function GetFundsModal({ open, setOpen }: ModalProps) {
+export default function GetFundsModal({ open, setOpen, token }: ModalProps) {
+  const { address: walletAddress } = useAccount();
+  const { address: tokenAddress, decimals } = token;
+
+  const { data: hash, writeContract } = useWriteContract();
+
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const amount = formData.get("amount") as string;
+    const toAddress = formData.get("toAddress") as string;
+
+    console.log("amount: ", amount);
+    console.log("walletAddress: ", walletAddress);
+    console.log(toAddress);
+
+    console.log(parseUnits(amount, parseInt(decimals)));
+
+    writeContract({
+      address: getAddress(tokenAddress),
+      abi: IERC20,
+      functionName: "mint",
+      args: [getAddress(toAddress), BigInt(amount)],
+    });
+  }
+
   return (
     <Modal open={open} setOpen={setOpen}>
       <Container>
-        <div className="border rounded py-4 px-8">
-          <p>Get test funds!</p>
-        </div>
+        {walletAddress && (
+          <div className="border rounded py-8 px-10 flex flex-col gap-y-4 w-[40rem]">
+            <p className="text-xl font-semibold">
+              Obtain {token.symbol} tokens
+            </p>
+
+            <p>Wallet Address: {addressResumer(walletAddress, 3)}</p>
+
+            <form
+              onSubmit={submit}
+              className="rounded flex flex-col gap-y-2 direct-children:p-2 direct-children:rounded"
+            >
+              <input
+                className="bg-gray-300"
+                name="amount"
+                type="number"
+                min="0"
+                step="1"
+                placeholder="Amount"
+                defaultValue={200}
+                required
+              />
+              <input
+                title="Your connected wallet address"
+                disabled
+                className="bg-gray-300 text-gray-500 cursor-not-allowed"
+                name="to"
+                placeholder="Address destinatary"
+                required
+                value={walletAddress}
+              />
+              <input type="hidden" name="toAddress" value={walletAddress} />
+              <button className="bg-green-400 p-2 mt-4" type="submit">
+                Get tokens
+              </button>
+
+              {hash && <div>Transaction Hash: {hash}</div>}
+            </form>
+          </div>
+        )}
       </Container>
     </Modal>
   );
