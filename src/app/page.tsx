4f76c6f15f921537980052610879b48d0e8d0fb9
NanezX <victor.nanezj@gmail.com>
2024-09-01 04:14:45 -0400

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 "use client";
-import ERC20Mock from "./components/contracts/erc20";
-import Seabrick from "./components/contracts/seabrick";
-import TabList from "./components/tabs/Tablist";
-import TabItem from "./components/tabs/TabItem";
-import Market from "./components/contracts/market";
 import ContractDetail from "./components/contracts/SeabrickNFTDetails";
 import LatestBuys from "./components/contracts/LatestBuys";
 import LatestTransfers from "./components/contracts/LatestTransfers";
