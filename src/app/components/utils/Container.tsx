9ba902e38ca0061bda18c89d77cf1ad0f967ce5a
NanezX <victor.nanezj@gmail.com>
2024-09-02 02:57:10 -0400

    lint

--- src/app/components/utils/Container.tsx
+++ src/app/components/utils/Container.tsx
@@ -1,4 +1,11 @@
-export default function Container({ children, bgColor = "bg-white" }: any) {
+interface ContainerProps {
+  children: React.ReactNode;
+  bgColor: `bg-${string}`;
+}
+export default function Container({
+  children,
+  bgColor = "bg-white",
+}: ContainerProps) {
   return (
     <div className={`${bgColor} shadow-lg border rounded`}>{children}</div>
   );
