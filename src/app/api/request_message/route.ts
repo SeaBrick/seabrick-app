513b4b5d709e3680778c0b9fd2688759f7c115c6
NanezX <victor.nanezj@gmail.com>
2024-10-03 22:29:04 -0400

    using env hash key for sign in messages

--- src/app/api/request_message/route.ts
+++ src/app/api/request_message/route.ts
@@ -3,6 +3,11 @@ import { type NextRequest, NextResponse } from "next/server";
 import { Address, isAddress } from "viem";
 import { createHmac } from "crypto";
 
+const keyHash = process.env.MESSAGE_HASH_KEY;
+if (!keyHash) {
+  throw new Error("Missing MESSAGE_HASH_KEY value");
+}
+
 export async function GET(request: NextRequest, response: NextResponse) {
   const address = request.nextUrl.searchParams.get("address");
 
@@ -18,10 +23,14 @@ export async function GET(request: NextRequest, response: NextResponse) {
 }
 
 function generateMessage(address: Address): string {
-  // FIXME: Should be an enviroment value with another value ofc
-  const keyHash = "f4ed77bbafb8113811a5c71941b18615";
+  const computedHmac = createHmac("sha256", keyHash!)
+    .update(address)
+    .digest("hex");
 
-  return createHmac("sha256", keyHash).update(address).digest("hex");
+  return `The seabrick site need to verify tour identity by signing this message with your wallet.
+
+  - Wallet address: ${address}
+  - ID: ${computedHmac}`;
 }
 
 export async function POST(request: Request, res: Response) {
