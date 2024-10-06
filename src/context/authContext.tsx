b6b19d11946be6f3c1706b319a974f1b6e8a91e2
NanezX <victor.nanezj@gmail.com>
2024-10-06 17:56:11 -0400

    auth context

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,56 +1,58 @@
 "use client";
 import { createClient } from "@/app/lib/supabase/client";
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { Session } from "@supabase/supabase-js";
-import { useRouter } from "next/navigation"; // For client-side routing
 
-const AuthContext = createContext<{ user: Session["user"] | null }>({
+const AuthContext = createContext<{
+  user: Session["user"] | null;
+  refetch: () => Promise<void>;
+}>({
   user: null,
+  refetch: async () => {},
 });
-
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<Session["user"] | null>(null);
   const supabaseClient = createClient();
-  const router = useRouter();
 
-  useEffect(() => {
-    // Check the session and set the user on mount
-    const getSession = async () => {
+  //   Refetch to update the user auth context
+  const refetch = useCallback(async () => {
+    const fetchUser = async () => {
       const {
         data: { session },
-        error,
       } = await supabaseClient.auth.getSession();
 
-      if (error) {
-        console.error("Error fetching session", error);
-      } else {
-        setUser(session?.user ?? null);
-      }
+      setUser(session?.user || null);
     };
 
-    getSession();
+    fetchUser();
+  }, [supabaseClient.auth]);
 
-    // Listen for auth changes to reflect in the UI
+  useEffect(() => {
+    // Call the function to get the user status
+    refetch();
+
+    // Use the authListener to listen auth changes
     const { data: authListener } = supabaseClient.auth.onAuthStateChange(
       (event, session) => {
-        if (session) {
-          setUser(session.user);
-        } else {
-          // Session is null when it expires
-          setUser(null);
-          // Redirect to login page if the session has expired
-          router.push("/login");
-        }
+        setUser(session?.user || null);
       }
     );
 
     return () => {
       authListener.subscription.unsubscribe();
     };
-  }, [supabaseClient, router]);
+  }, [refetch, supabaseClient.auth]);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, refetch }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
