7091f7b665053d7b7ea9cf2b81c593acc5ca3c74
NanezX <victor.nanezj@gmail.com>
2024-10-06 18:37:14 -0400

    wip: signin with wallet

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "../lib/supabase/server";
+import { verifySignature } from "../api/utils";
+import { Address, Hex } from "viem";
 
 export async function login(formData: FormData) {
   const supabase = createClient();
@@ -57,4 +59,19 @@ export async function signup(formData: FormData) {
   console.log("coool xd");
   revalidatePath("/", "layout");
   redirect("/");
+}
+
+export async function signinWithWallet(formData: FormData) {
+  const address = formData.get("address")?.toString() as string;
+  const signature = formData.get("signature")?.toString() as string;
+
+  const isValidSignature = await verifySignature(
+    address! as Address,
+    signature! as Hex
+  );
+
+  if (!isValidSignature) {
+    console.log("not valid signature");
+    redirect("/error");
+  }
 }
