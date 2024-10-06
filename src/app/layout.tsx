01f1355c9fac161b2d7b2be41f88fb386c1dee9b
NanezX <victor.nanezj@gmail.com>
2024-10-06 16:05:01 -0400

    layout wiht auth accounts provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import { Footer } from "./components/layout/Footer";
 import { ContractProvider } from "@/context/contractContext";
 import { AccountProvider } from "@/context/accountContext";
 import { AggregatorsProvider } from "@/context/aggregatorsContext";
+import { AuthProvider } from "@/context/authContext";
 
 export const metadata: Metadata = {
   title: "Seabrick Demo App",
@@ -26,22 +27,24 @@ export default function RootLayout({
   // const initialState = cookieToInitialState(config, headers().get("cookie"));
   const initialState = cookieToInitialState(config);
   return (
-    <AppKitProvider initialState={initialState}>
-      <ContractProvider>
-        <AggregatorsProvider>
-          <AccountProvider>
-            <html lang="en">
-              <body className="flex flex-col min-h-svh">
-                <Navbar />
-                <main className="flex-grow">
-                  <SplashScreen>{children}</SplashScreen>
-                </main>
-                <Footer />
-              </body>
-            </html>
-          </AccountProvider>
-        </AggregatorsProvider>
-      </ContractProvider>
-    </AppKitProvider>
+    <AuthProvider>
+      <AppKitProvider initialState={initialState}>
+        <ContractProvider>
+          <AggregatorsProvider>
+            <AccountProvider>
+              <html lang="en">
+                <body className="flex flex-col min-h-svh">
+                  <Navbar />
+                  <main className="flex-grow">
+                    <SplashScreen>{children}</SplashScreen>
+                  </main>
+                  <Footer />
+                </body>
+              </html>
+            </AccountProvider>
+          </AggregatorsProvider>
+        </ContractProvider>
+      </AppKitProvider>
+    </AuthProvider>
   );
 }
