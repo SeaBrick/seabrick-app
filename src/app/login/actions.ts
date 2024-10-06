e82de3168640b571ba2b0879c0d97e38ab451fae
NanezX <victor.nanezj@gmail.com>
2024-10-06 15:23:34 -0400

    wip: log

---
+++ src/app/login/actions.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "../lib/supabase/server";
+
+export async function login(formData: FormData) {
+  const supabase = createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    console.log("xdd error");
+    console.log(error);
+    redirect("/error");
+  }
+
+  console.log("coool xd");
+  revalidatePath("/", "layout");
+  redirect("/buy");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error, data: aver } = await supabase.auth.signUp(data);
+
+  if (error) {
+    console.log(error);
+    console.log(aver);
+    redirect("/error");
+  }
+
+  console.log("coool xd");
+  revalidatePath("/", "layout");
+  redirect("/buy");
+}
