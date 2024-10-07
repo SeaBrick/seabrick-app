bbfd47da9c934ce43b8a8d15c6aa6b10f78cd843
NanezX <victor.nanezj@gmail.com>
2024-10-07 02:42:25 -0400

    signout anon user when disconnect wallet

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -8,6 +8,7 @@ import {
   useState,
 } from "react";
 import { Session } from "@supabase/supabase-js";
+import { useAccountEffect } from "wagmi";
 
 const AuthContext = createContext<{
   user: Session["user"] | null;
@@ -19,6 +20,15 @@ const AuthContext = createContext<{
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<Session["user"] | null>(null);
   const supabaseClient = createClient();
+
+  useAccountEffect({
+    onDisconnect() {
+      async function signOut() {
+        await supabaseClient.auth.signOut();
+      }
+      signOut();
+    },
+  });
 
   //   Refetch to update the user auth context
   const refetch = useCallback(async () => {
@@ -35,13 +45,13 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     refetch();
-  }, []);
+  }, [refetch]);
 
   useEffect(() => {
     // Use the authListener to listen auth changes
     const { data: authListener } = supabaseClient.auth.onAuthStateChange(
       (event, session) => {
-        console.log("onAuthStateChange.event: ", event);
+        console.log("onAuthStateChange.event: ", event, "- session: ", session);
         setUser(session?.user || null);
       }
     );
