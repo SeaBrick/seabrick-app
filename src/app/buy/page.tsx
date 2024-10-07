36b6f231ceed31fa4ea5f6e4676c2698ba8840ff
NanezX <victor.nanezj@gmail.com>
2024-10-07 03:34:59 -0400

    wip: stripe

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 import React, { Suspense } from "react";
-import RequireWallet from "@/app/components/utils/RequireWallet";
+// import RequireWallet from "@/app/components/utils/RequireWallet";
 import BuyNFT from "./BuyNFT";
 import PageLoaderSpinner from "../components/spinners/PageLoaderSpinner";
+import { useAuth } from "@/context/authContext";
+import { useRouter } from "next/navigation";
 
 export default function BuyPage() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  if (!user) {
+    // TODO: Add little modal with message that need to login
+    router.push("/login");
+    return;
+  }
+
   return (
-    <RequireWallet>
-      <div className="w-1/2 mx-auto">
-        <p className="text-3xl font-bold mb-8">Buy Seabrick NFT</p>
+    <div className="w-1/2 mx-auto">
+      <p className="text-3xl font-bold mb-8">Buy Seabrick NFT</p>
+
+      {user.user_metadata.type == "email" && <p>Buy with stripe</p>}
+
+      {user.user_metadata.type == "wallet" && (
         <Suspense
           fallback={
             <div className="py-24 my-auto">
@@ -18,7 +32,7 @@ export default function BuyPage() {
         >
           <BuyNFT />
         </Suspense>
-      </div>
-    </RequireWallet>
+      )}
+    </div>
   );
 }
