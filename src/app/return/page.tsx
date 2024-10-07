6fe915299660f46e742fef940d549938d1403462
NanezX <victor.nanezj@gmail.com>
2024-10-07 15:53:49 -0400

    center text on return page

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -27,13 +27,15 @@ export default function Return() {
 
   if (status === "complete") {
     return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to{" "}
-          {customerEmail}. If you have any questions, please email{" "}
-          <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-      </section>
+      <div className="w-1/2 mx-auto text-justify">
+        <section id="success">
+          <p>
+            We appreciate your business! A confirmation email will be sent to{" "}
+            {customerEmail}. If you have any questions, please email{" "}
+            <a href="mailto:orders@example.com">orders@example.com</a>.
+          </p>
+        </section>
+      </div>
     );
   }
 
