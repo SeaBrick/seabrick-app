58ed3b6e4a75deab7835c547cd58638fa207108a
NanezX <victor.nanezj@gmail.com>
2024-10-16 18:44:40 -0400

    different tabs

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -30,19 +30,38 @@ export default function AccountDetailsPage() {
 
   return (
     <div className="w-1/2 mx-auto">
-      <h1>Account Details Page</h1>
-      <p>Welcome to the account details section.</p>
+      <p className="text-3xl font-bold mb-8">Account</p>
 
       <TabGroup selectedIndex={selectedIndex} onChange={setSelectedIndex}>
         <TabList className="flex gap-4">
-          <Tab className="rounded-full py-1 px-3 focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white">
-            <Link href="/account">Account details</Link>
+          <Tab className="rounded-full py-1 px-3 focus:outline-none data-[selected]:bg-seabrick-blue/10 data-[hover]:bg-seabrick-blue/5 data-[selected]:data-[hover]:bg-seabrick-blue/10 data-[focus]:outline-1 data-[focus]:outline-white">
+            <Link prefetch={true} href="/account">
+              Account details
+            </Link>
           </Tab>
-          <Tab className="rounded-full py-1 px-3 focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white">
-            <Link href="/account?tab=transactions">Account transactions</Link>
+          <Tab className="rounded-full py-1 px-3 focus:outline-none data-[selected]:bg-seabrick-blue/10 data-[hover]:bg-seabrick-blue/5 data-[selected]:data-[hover]:bg-seabrick-blue/10 data-[focus]:outline-1 data-[focus]:outline-white">
+            <Link prefetch={true} href="/account?tab=transactions">
+              Account transactions
+            </Link>
           </Tab>
         </TabList>
+        <TabPanels className="mt-3">
+          <TabPanel className="rounded-xl bg-seabrick-blue/5 p-3">
+            <AccountDetails />
+          </TabPanel>
+          <TabPanel className="rounded-xl bg-seabrick-blue/5 p-3">
+            <AccountTransactions />
+          </TabPanel>
+        </TabPanels>
       </TabGroup>
     </div>
   );
 }
+
+const AccountDetails: React.FC = () => {
+  return <>Account details</>;
+};
+
+const AccountTransactions: React.FC = () => {
+  return <>Account transactions</>;
+};
