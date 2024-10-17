6ee27ecc9ff09d188966ee48469a65d583f3dad9
NanezX <victor.nanezj@gmail.com>
2024-10-17 03:28:12 -0400

    supoort for disable button

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -2,16 +2,18 @@ import { useFormStatus } from "react-dom";
 
 interface SubmitButtonProps {
   text?: string;
+  disable: boolean;
 }
-export default function SubmitButton({ text }: SubmitButtonProps) {
+export default function SubmitButton({ text, disable }: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
     <button
       type="submit"
       aria-disabled={pending}
-      disabled={pending}
-      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none  hover:bg-gray-500"
+      disabled={pending || disable}
+      title={pending || disable ? "No changes to save" : undefined}
+      className="inline-flex items-center gap-2 disabled:bg-gray-500 disabled:cursor-not-allowed rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none  hover:bg-gray-500"
     >
       {pending ? "Loading..." : text || "Submit"}
     </button>
