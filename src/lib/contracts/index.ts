62ba718784808e1296b7fc6fa24de6f3eedb1d6e
NanezX <victor.nanezj@gmail.com>
2024-10-24 04:15:16 -0400

    addresses ttypes updated

--- src/lib/contracts/index.ts
+++ src/lib/contracts/index.ts
@@ -1,4 +1,6 @@
-export const addresses = {
+import { Address } from "viem";
+
+export const addresses: { SeabrickNFT: Address; SeabrickMarket: Address } = {
   SeabrickNFT: "0x056b74b11c59b64cc532eed4bce181b3ad5412e0",
   SeabrickMarket: "0x35288e3c7dbb0a422dc6e71765617c2af0b10ad2",
 };
