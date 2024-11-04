633fd23355f8bd0f780de7e3dd9f5680d6343728
NanezX <victor.nanezj@gmail.com>
2024-11-04 18:23:04 -0400

    comment to remember change the env

--- src/app/api/stripe/index.ts
+++ src/app/api/stripe/index.ts
@@ -1,5 +1,8 @@
 import Stripe from "stripe";
 
+
+// TODO: Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
 const stripe_secret_key = process.env.STRIPE_SECRET_KEY;
 if (!stripe_secret_key) {
   throw new Error("Missing STRIPE_SECRET_KEY value");
