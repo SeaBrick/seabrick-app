2832c2981adeef884773dfb565a5792c3f42d14d
NanezX <victor.nanezj@gmail.com>
2024-11-04 20:08:27 -0400

    webhook route updated

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -5,13 +5,41 @@ import { NextResponse, NextRequest } from "next/server";
 import { isEmpty } from "lodash";
 import { stripe } from "@/app/api/stripe";
 import { type Stripe } from "stripe";
+import { FulfillCheckoutResp } from "@/lib/interfaces/api";
+import { type SupabaseClient } from "@supabase/supabase-js";
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_FULFILLMENT_SECRET ?? "";
 if (!endpointSecret) {
   throw new Error("Missing STRIPE_WEBHOOK_FULFILLMENT_SECRET value");
 }
 
-async function fulfillCheckout(sessionId: string) {
+async function updateSessionFulfillment(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  supabaseClient: SupabaseClient<any, "public", any>,
+  sessionId: string,
+  status: boolean
+) {
+  const { error: updateError } = await supabaseClient
+    .from("stripe_checkout_sessions")
+    .update({
+      fulfilled: status,
+    })
+    .eq("session_id", sessionId);
+
+  if (updateError) {
+    console.error(
+      "Update session checkout fulfilled status failed. ID: ",
+      sessionId
+    );
+    return false;
+  }
+
+  return true;
+}
+
+async function fulfillCheckout(
+  sessionId: string
+): Promise<FulfillCheckoutResp> {
   // Create a supabase client with service role
   const supabaseClient = createClient(true);
 
@@ -25,19 +53,26 @@ async function fulfillCheckout(sessionId: string) {
     .eq("session_id", sessionId)
     .single();
 
-  // `PGRST116` is `The result contains 0 rows` error (since the table have a unique constraint at session_id)
+  // `PGRST116` is `The result contains 0 rows` error. Since the table have a unique constraint at session_id,
+  // this means that the entry is not created yet. Should not happen
   // If we get that error, means that the session is not found (not fulfilled)
   if (checkError && checkError.code != "PGRST116") {
     // TODO: Not throw, just return something
     console.error("Error checking session status:", checkError.message);
-    throw new Error("Failed to check session status");
+    // throw new Error("Failed to check session status");
+
+    return {
+      isMinted: false,
+      message: `Session not saved on database. ID: ${sessionId}`,
+    };
   }
 
   // If the session has already been fulfilled, return early
   if (sessionData?.fulfilled) {
-    // TODO: Return something
-    console.log("Session already fulfilled");
-    return;
+    // We return isMinted as true since it's already fulfilled, which means
+    // that the NFTs related to this stripe session were already minted
+    console.info("Session already fulfilled. ID: ", sessionId);
+    return { isMinted: true };
   }
 
   // Retrieve the Checkout Session from the API with line_items expanded
@@ -52,8 +87,7 @@ async function fulfillCheckout(sessionId: string) {
     // So we get the `quantity` bought directly
     const quantity = checkoutSession.line_items?.data[0].quantity;
 
-    // User metadata from supabase
-    // TODO: Use this data to mint or get data to mint and assign the nfts
+    // User metadata from the User that made the Buy
     const userMetadata = checkoutSession.metadata;
 
     if (
@@ -61,48 +95,75 @@ async function fulfillCheckout(sessionId: string) {
       isEmpty(userMetadata) ||
       !userMetadata.user_id
     ) {
-      console.error("User metadata is empty");
-      throw new Error("Failed to mark session as fulfilled");
+      console.error("No user metadata found for this session: ", sessionId);
+      return {
+        isMinted: false,
+        message: "No user metadata found for this session",
+      };
     }
 
-    console.log(
-      `Minting... ${quantity} NFTs to this user: ${userMetadata?.email}`
+    if (!quantity) {
+      console.error("No quantity defined for this session: ", sessionId);
+      return {
+        isMinted: false,
+        message: "No quantity defined for this session",
+      };
+    }
+
+    // We mark the session as fulfilled before sending the transaction
+    const statusUpdated = await updateSessionFulfillment(
+      supabaseClient,
+      sessionId,
+      true
     );
 
-    const isMinted = await mintSeabrickTokens(
-      "0x304152266BD626c6D718ca03385F4498D933D168",
-      quantity ?? 1
-    );
+    // If we get an error on this, we do not continue.
+    // This way we avoid minting multiple times or any security issue
+    if (!statusUpdated) {
+      return {
+        isMinted: false,
+        message: `Cannot mark the session as fulfilled: ID: ${sessionId}`,
+      };
+    }
+
+    // Mint tokens to the our "minter" address, so we can identify them
+    const isMinted = await mintSeabrickTokens(quantity);
 
     if (isMinted) {
-      // Mark the session as fulfilled in Supabase
-      const { error: updateError } = await supabaseClient
-        .from("stripe_checkout_sessions")
-        .upsert({
-          session_id: sessionId,
-          fulfilled: true,
-          user_id: userMetadata.user_id,
-        });
+      // Everything went good, we return that was minted
+      // TODO: Perform fulfillment of the line items, like mintint the total amount and saving the ids
+      // to the DB and the vault app address (NOT for the vault for the collected)
 
-      if (updateError) {
-        console.error(
-          "Error marking session as fulfilled:",
-          updateError.message
-        );
-        throw new Error("Failed to mark session as fulfilled");
-      }
-
-      console.log("Session fulfillment completed.");
-    } else {
-      // TODO: Not throw, just return something
-      throw new Error("Minting failed.");
+      return { isMinted };
     }
 
-    // TODO: Perform fulfillment of the line items, like mintint the total amount and saving the ids
-    // to the DB and the vault app address (NOT for the vault for the collected)
-    // TODO: Record/save fulfillment status for this
-    // Checkout Session
+    // If not minted
+    else {
+      const statusUpdated = await updateSessionFulfillment(
+        supabaseClient,
+        sessionId,
+        false
+      );
+
+      if (!statusUpdated) {
+        // Something bad happened, must be checked
+        console.error(
+          "Checkout session not minted and not set status as false again. Session ID: ",
+          sessionId
+        );
+      }
+
+      return {
+        isMinted: false,
+        message: "Tokens were not minted for this session",
+      };
+    }
   }
+
+  return {
+    isMinted: false,
+    message: "The payment_status of this session is unpaid",
+  };
 }
 
 export async function POST(req: NextRequest) {
@@ -141,8 +202,20 @@ export async function POST(req: NextRequest) {
   }
 
   if (event.type === "checkout.session.completed") {
+    // We get the session object
     const session = event.data.object;
+
+    // Run to fulfill the checkout
     const isFulfilled = await fulfillCheckout(session.id);
+
+    // If the session was not fulfilled, then we return an error with the message
+    // Which will case that this session will be tried after a while
+    if (isFulfilled.isMinted === false) {
+      return NextResponse.json(
+        { error: `Webhook Error: ${isFulfilled.message}` },
+        { status: 400 }
+      );
+    }
   }
 
   // Return a response to acknowledge receipt of the event
