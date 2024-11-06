376590d948dd6fb3347cf799f783910cf237e67e
unknown <jose.c.prieto.c2015@gmail.com>
2024-11-06 14:28:50 -0400

    table done

--- src/app/order-details/page.tsx
+++ src/app/order-details/page.tsx
@@ -3,8 +3,8 @@ import Table from "@/components/table/TableTest"
 export default function OrderDetailsMain() {
   return (
     <>
-      <div className="w-3/4 p-6 bg-white rounded-[10px] justify-center items-center gap-2.5 flex m-auto flex-col">
-        <div className="self-stretch h-[65px] flex-col justify-start items-start gap-6 flex">
+      <div className="w-3/4 min-w-[200px] box-content p-6 bg-white rounded-[10px] justify-center items-center gap-2.5 flex m-auto flex-col">
+        <div className="self-stretch h-auto flex-col justify-start items-start gap-6 flex">
           <div className="self-stretch justify-start items-center gap-4 inline-flex">
             <Image
               className="w-[65px] h-[62px]"
@@ -13,16 +13,14 @@ export default function OrderDetailsMain() {
               width={65}
               height={62}
             />
-            <div className="w-[332px] h-[65px] flex-col justify-between items-start inline-flex">
-              <div className="w-[254px] justify-start items-center gap-2.5 inline-flex">
+            <div className="flex-col justify-between items-start inline-flex">
+              <div className="justify-start items-center gap-2.5 inline-flex">
                 <div className="text-[#8a8a8f] text-[15px] font-normal font-['Noto Sans']">
                   Seabrick NFT
                 </div>
               </div>
-              <div className="self-stretch justify-start items-end gap-2.5 inline-flex">
-                <div className="w-[178px] h-[33px] text-[#323232] text-4xl font-normal font-['Noto Sans'] w-auto">
-                  100,00 US$
-                </div>
+              <div className="self-stretch justify-start items-end gap-2.5 inline-flex text-[#323232] text-4xl font-normal font-['Noto Sans'] w-auto">
+                <span>100,00 US$</span>
               </div>
             </div>
           </div>
@@ -38,19 +36,19 @@ export default function OrderDetailsMain() {
             {
               tokenId: 2,
               status: "true",
-              hash: "0x00000000",
+              hash: "0x0000000000000000000",
               date: "01/01/2024",
             },
             {
               tokenId: 3,
               status: "cccc",
-              hash: "0x100000000",
+              hash: "0x1000000000000000000",
               date: "01/02/2024",
             },
             {
               tokenId: 4,
               status: "asdasd",
-              hash: "0x000100000",
+              hash: "0x0001000000000000000000",
               date: "01/03/2024",
             },
           ]}
--- src/components/table/TableTest.tsx
+++ src/components/table/TableTest.tsx
@@ -3,16 +3,30 @@ interface TableColumn {
   label: string
 }
 
+function truncateString(str: string) {
+  // Si la cadena tiene menos de 5 caracteres, retorna la cadena completa
+  if (str.length <= 13) {
+    return str
+  }
+
+  // Toma los primeros 5 caracteres y los últimos
+  const firsts = str.slice(0, 5)
+  const lasts = str.slice(-5)
+
+  // Combina los primeros 5, los puntos suspensivos y los últimos
+  return firsts + "..." + lasts
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Table({ columns, data }: { columns: TableColumn[]; data: any[] }) {
   return (
-    <div className="overflow-auto w-full">
-      <table className="w-full min-w-fit bg-white rounded-[10px] flex-col justify-start items-start gap-2.5 flex mt-[20px] table-fixed">
-        <thead className="w-full h-auto px-6 py-4 bg-[#efeff4] flex-col justify-start items-start gap-5 flex rounded-t-[10px]">
-          <tr className="items-center inline-flex w-full">
+    <div className="overflow-auto w-full mt-4 rounded-t-[10px]">
+      <table className="w-full min-w-[500px] bg-white gap-2.5 table-fixed">
+        <thead className="w-full bg-[#efeff4] gap-5">
+          <tr className="w-full table-row">
             {columns.map((column) => (
               <th
-                className="text-black w-full text-justify text-xs font-normal font-['Noto Sans']"
+                className="text-black w-full text-justify text-xs font-normal font-['Noto Sans'] table-cell p-4"
                 key={column.key}
               >
                 {column.label}
@@ -20,17 +34,26 @@ function Table({ columns, data }: { columns: TableColumn[]; data: any[] }) {
             ))}
           </tr>
         </thead>
-        <tbody className="w-full px-6 pt-[17px] flex-col justify-start items-end gap-4 flex min-w-fit">
+        <tbody className="w-full px-6 pt-[17px] gap-4 min-w-fit">
           {data.map((row) => (
-            <tr className="w-full items-start inline-flex" key={row.id}>
-              {columns.map((column) => (
-                <td
-                  className="text-black w-full text-justify text-xs font-normal font-['Noto Sans']"
-                  key={column.key}
-                >
-                  {row[column.key]}
-                </td>
-              ))}
+            <tr
+              className="w-full table-row border-b-[1px] border-[#efeff4]"
+              key={row.id}
+            >
+              {columns.map((column) => {
+                if (column.key === "hash" || column.key == "address") {
+                  row[column.key] = truncateString(row[column.key])
+                  console.log(row[column.key])
+                }
+                return (
+                  <td
+                    className="text-black w-full text-justify text-xs font-normal font-['Noto Sans'] table-cell p-4"
+                    key={column.key}
+                  >
+                    {row[column.key]}
+                  </td>
+                )
+              })}
             </tr>
           ))}
         </tbody>
