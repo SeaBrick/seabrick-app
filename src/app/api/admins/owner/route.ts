17e9ebdef5c9c777faaff51ebd424135e7a8b92c
NanezX <victor.nanezj@gmail.com>
2024-11-05 20:37:30 -0400

    transfer ownership

---
+++ src/app/api/admins/owner/route.ts
@@ -0,0 +1,93 @@
+"use server";
+import { createClient } from "@/lib/supabase/server";
+import { getUserRole } from "@/lib/utils/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const supabaseClient = createClient();
+
+  const {
+    data: { user },
+  } = await supabaseClient.auth.getUser();
+
+  if (!user) {
+    return NextResponse.json(
+      { error: "Unauthorized", details: "No user logged" },
+      { status: 401 }
+    );
+  }
+
+  const userRole = await getUserRole(supabaseClient);
+
+  if (userRole !== "owner") {
+    return NextResponse.json(
+      { error: "Unauthorized", details: "No access" },
+      { status: 401 }
+    );
+  }
+
+  let email: string;
+
+  try {
+    // Log request headers and method to confirm it's a POST with JSON content-type
+
+    const body = await request.json();
+    if (!body.email) {
+      throw new Error("Not email in the request body");
+    }
+
+    email = body.email;
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Bad Request", details: "Malformed request" },
+      { status: 400 }
+    );
+  }
+
+  const { data: dataUser, error: errorUser } = await supabaseClient
+    .rpc("get_user_id_by_email", {
+      email: email,
+    })
+    .returns()
+    .single<{ id: string }>();
+
+  // 'PGRST116' code is 'JSON object requested, multiple (or no) rows returned'
+  // We handle that as not email found
+  if (errorUser && errorUser.code != "PGRST116") {
+    console.error("Error when getting the user: ");
+    console.error(errorUser);
+    return NextResponse.json(
+      { error: "Internal server error", details: errorUser.message },
+      { status: 500 }
+    );
+  }
+
+  if ((errorUser && errorUser.code == "PGRST116") || !dataUser) {
+    return NextResponse.json(
+      { error: "Not found", details: "User with this email not found" },
+      { status: 404 }
+    );
+  }
+
+  const { error } = await supabaseClient.from("user_roles").upsert(
+    [
+      { user_id: dataUser.id, role: "owner" },
+      { user_id: user.id, role: "admin" },
+    ],
+    { ignoreDuplicates: false, onConflict: "user_id" }
+  );
+
+  if (error) {
+    console.error("Transfering ownership failed");
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error", details: error.message },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({
+    message: "ok",
+  });
+}
