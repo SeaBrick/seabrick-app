e11d4e8da73df441f05143c71c97ea78e00c4937
unknown <jose.c.prieto.c2015@gmail.com>
2024-11-06 22:34:54 -0400

    smalling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -103,20 +103,20 @@ export default function Dashboard() {
   ]
   return (
     <>
-      <div className="w-full p-3">
-        <div className="w-full justify-start items-center gap-4 inline-flex mb-[38px]">
+      <div className="w-full px-3">
+        <div className="w-full justify-start items-center gap-2 inline-flex mb-4">
           <Image
             src={`/brick.png`}
             alt="user-image"
-            height={71}
-            width={71}
+            height={45}
+            width={48}
             className="rounded-[100px]"
           />
-          <div className="h-[70px] flex-col justify-start items-start gap-px inline-flex">
-            <div className="self-stretch text-[#666666] text-[17px] font-normal font-['Noto Sans']">
+          <div className="h-[70px] flex-col justify-center items-start gap-px inline-flex">
+            <div className="self-stretch text-[#666666] text-[9px] font-normal font-['Noto Sans']">
               Welcome Back!
             </div>
-            <div className="text-black text-4xl font-normal font-['Noto Sans']">
+            <div className="text-black text-2xl font-normal font-['Noto Sans']">
               {userName}
             </div>
           </div>
@@ -124,16 +124,16 @@ export default function Dashboard() {
         <div className="w-full gap-3 flex">
           {/* left side */}
           <div className="w-1/3 p-6 bg-white rounded-[10px] justify-start items-center gap-2.5 flex flex-col">
-            <div className="flex-col justify-start items-start gap-6 inline-flex w-full mb-6">
+            <div className="flex-col justify-start items-start gap-3 inline-flex w-full mb-4">
               {/* top with texts */}
               <div className="self-stretch flex-col justify-between items-start inline-flex">
                 <div className="justify-start items-center gap-2.5 inline-flex">
-                  <div className="text-[#333333] text-[15px] font-normal font-['Noto Sans']">
+                  <div className="text-[#333333] text-xs font-normal font-['Noto Sans']">
                     Total Supply
                   </div>
                 </div>
                 <div className="self-stretch justify-start items-end gap-2.5 inline-flex">
-                  <div className="text-[#333333] text-4xl font-normal font-['Noto Sans']">
+                  <div className="text-[#333333] text-2xl font-normal font-['Noto Sans']">
                     {totalSupply} Seabrick NFTs
                   </div>
                 </div>
@@ -141,23 +141,23 @@ export default function Dashboard() {
               {/* bottom with button */}
               <div className="justify-start items-center gap-2 inline-flex">
                 <div className="justify-start items-center gap-2 flex">
-                  <button className="p-[17px] bg-[#2069a0] rounded-[5px] justify-start items-center gap-2.5 flex text-right text-white text-sm font-normal font-['Noto Sans']">
+                  <button className="p-2 bg-[#2069a0] rounded-[5px] justify-start items-center gap-2.5 flex text-right text-white text-sm font-normal font-['Noto Sans']">
                     Mint New Tokens
                   </button>
                 </div>
               </div>
             </div>
             {/* left bottom */}
-            <div className="flex-col justify-start items-start gap-6 inline-flex w-full">
+            <div className="flex-col justify-start items-start gap-4 inline-flex w-full">
               {/*  filter and description  */}
               <div className="flex-col justify-start items-start gap-2 flex">
                 {/* filter and date  */}
                 <div className="self-stretch justify-start items-center gap-[5px] inline-flex">
-                  <div className="text-[#8a8a8f] text-sm font-normal font-['Noto Sans']">
+                  <div className="text-[#8a8a8f] text-xs font-normal font-['Noto Sans']">
                     Minted Tokens
                   </div>
-                  <div className="justify-start items-center gap-[5px] flex">
-                    <div className="text-[#333333] text-sm font-normal font-['Noto Sans']">
+                  <div className="justify-start items-center gap-[3px] flex">
+                    <div className="text-[#333333] text-xs font-normal font-['Noto Sans']">
                       November 2024
                       {/* calendar??? */}
                     </div>
@@ -165,7 +165,7 @@ export default function Dashboard() {
                 </div>
                 {/* small stats */}
                 <div className="self-stretch justify-start items-center gap-4 inline-flex">
-                  <div className="text-[#333333] text-xl font-normal font-['Noto Sans']">
+                  <div className="text-[#333333] text-l font-normal font-['Noto Sans']">
                     {thisMontQuantity > 0 ? "+" : ""}
                     {thisMontQuantity}
                   </div>
