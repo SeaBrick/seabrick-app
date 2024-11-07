696885abfb71ae616cf8eb8923708cd0544d1358
Rightles <ricardodiaz1999@gmail.com>
2024-11-07 12:29:26 -0400

    Png to webp convert ⚡️

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,6 @@ import { createClient } from "@/lib/supabase/client";
 import SigninWalletModal from "@/components/modals/SigninWalletModal";
 import { useFormState } from "react-dom";
 import Image from "next/image";
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import Link from "next/link";
 import SubmitButton from "@/components/buttons/SubmitButton";
 import { isEmpty } from "lodash";
@@ -290,7 +289,7 @@ export default function LoginPage() {
       <div className="w-full h-[80vh] md:h-screen relative bg-[#f6f6f6] flex justify-center">
         <Image
           className="w-full h-[200px] md:h-[414px] left-0 top-1 absolute z-0 rounded-bl-[50px] rounded-br-[50px]"
-          src={`/login-bg.png`}
+          src={`/login-bg.webp`}
           alt="banner"
           width={1920}
           height={414}
