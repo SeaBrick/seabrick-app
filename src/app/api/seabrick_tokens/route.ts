9a36cd48bedbf2fd9ff524d3f6837c6144e17917
NanezX <victor.nanezj@gmail.com>
2024-11-06 20:37:22 -0400

    claim tokens to user wallet address

--- src/app/api/seabrick_tokens/route.ts
+++ src/app/api/seabrick_tokens/route.ts
@@ -1,13 +1,15 @@
 "use server";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 import { type StripeBuysByUserResponse } from "@/lib/interfaces/api";
 import { type PostgrestSingleResponse } from "@supabase/supabase-js";
 import { type SupabaseClient } from "@supabase/supabase-js";
+import { transferFromVault } from "@/lib/contracts/transactions";
+import { checkAddress } from "@/lib/utils";
 
 // Maybe we can add pagination logic
 async function getUserStripeBuys(
-  supabaseClient: SupabaseClient<any, "public", any>,
+  supabaseClient: SupabaseClient<never, "public", never>,
   userId: string,
   options: {
     orderBy?: string;
@@ -69,7 +71,7 @@ export async function GET() {
   });
 }
 
-export async function PATCH(request: NextRequest) {
+export async function PATCH() {
   const supabaseClient = createClient();
 
   const {
@@ -83,17 +85,83 @@ export async function PATCH(request: NextRequest) {
     );
   }
 
-  const { data, error } = await getUserStripeBuys(supabaseClient, user.id, {
-    onlyNotClaimed: true,
-  });
+  const { data: walletData, error: walletError } = await supabaseClient
+    .from("wallet_users")
+    .select("address")
+    .eq("user_id", user.id)
+    .single<{ address: string }>();
 
-  if (error) {
+  if (walletError) {
     return NextResponse.json(
-      { error: "Internal server error", details: error.message },
+      {
+        error: "No wallet linked",
+        details: "The user do not have a linked wallet to his account",
+      },
+      { status: 404 }
+    );
+  }
+
+  const { address: userWalletAddress } = walletData;
+
+  if (!userWalletAddress || !checkAddress(userWalletAddress)) {
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+        details: "The wallet address linked of the user is not a valid address",
+      },
       { status: 500 }
     );
   }
 
-  // TODO: Transfer batch based on the ids
-  // const a = data;
+  const { data: buysData, error: buysError } = await getUserStripeBuys(
+    supabaseClient,
+    user.id,
+    {
+      onlyNotClaimed: true,
+      limit: 5,
+    }
+  );
+
+  if (buysError) {
+    return NextResponse.json(
+      { error: "Internal server error", details: buysError.message },
+      { status: 500 }
+    );
+  }
+
+  // return NextResponse.json({
+  //   walletData,
+  //   walletError,
+  //   buysData,
+  // });
+
+  if (buysData.length === 0) {
+    return NextResponse.json(
+      {
+        error: "No tokens to claim",
+        details: "The user do not have tokens available to be claimed",
+      },
+      { status: 404 }
+    );
+  }
+
+  // Conver the tokens id from string to BigInt
+  const tokenIds = buysData.map((buy_) => BigInt(buy_.token_id));
+
+  const resp = await transferFromVault(tokenIds, userWalletAddress);
+
+  if (resp.isTransfer === false) {
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+        details: "Failed to transfer the tokens to the user wallet",
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({
+    message: "ok",
+    txHash: resp.txHash,
+  });
 }
