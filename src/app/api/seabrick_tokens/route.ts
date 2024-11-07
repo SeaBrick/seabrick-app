498bf575f4b9e15e55ef3c91c9a33e3b1b8f6e95
NanezX <victor.nanezj@gmail.com>
2024-11-07 00:18:53 -0400

    claim tokens endpoint

--- src/app/api/seabrick_tokens/route.ts
+++ src/app/api/seabrick_tokens/route.ts
@@ -21,7 +21,7 @@ async function getUserStripeBuys(
   // Deconstruc the options
   const {
     orderBy = "created_at",
-    ascending = false,
+    ascending = true,
     limit = 10,
     onlyNotClaimed = false,
   } = options;
@@ -118,7 +118,7 @@ export async function PATCH() {
     user.id,
     {
       onlyNotClaimed: true,
-      limit: 5,
+      limit: 10,
     }
   );
 
@@ -128,12 +128,6 @@ export async function PATCH() {
       { status: 500 }
     );
   }
-
-  // return NextResponse.json({
-  //   walletData,
-  //   walletError,
-  //   buysData,
-  // });
 
   if (buysData.length === 0) {
     return NextResponse.json(
@@ -145,23 +139,90 @@ export async function PATCH() {
     );
   }
 
-  // Conver the tokens id from string to BigInt
-  const tokenIds = buysData.map((buy_) => BigInt(buy_.token_id));
+  // Mark them as claimed before transfering the tokens to avoid any possible attack
+  const buysIdToUpdate = buysData.map((buyData_) => buyData_.id);
 
-  const resp = await transferFromVault(tokenIds, userWalletAddress);
+  // Sending the update
+  const {
+    error: updateError,
+    data: updateData,
+    count: updateCount,
+  } = await supabaseClient
+    .from("stripe_buys")
+    .update({ claimed: true })
+    .in("id", buysIdToUpdate)
+    .select();
 
-  if (resp.isTransfer === false) {
+  // If we cannot update the claimed status, we fail
+  if (updateError || updateData.length === 0 || updateCount == 0) {
+    console.error(updateError);
     return NextResponse.json(
       {
         error: "Internal server error",
-        details: "Failed to transfer the tokens to the user wallet",
+        details: "Failed to update the claimed status",
       },
       { status: 500 }
     );
   }
 
+  // Convert the tokens id from string to BigInt
+  const tokenIds = buysData.map((buy_) => BigInt(buy_.token_id));
+  const tokenIdsString = tokenIds.map((val_) => val_.toString());
+
+  // We send the transfer request
+  const transferResp = await transferFromVault(tokenIds, userWalletAddress);
+
+  // If the transfer failed, we change again the status
+  if (transferResp.isTransfer === false) {
+    // update_claimed_status
+    const { error: unctionFallbackError } = await createClient(true).rpc(
+      "update_claimed_status",
+      {
+        ids: buysIdToUpdate,
+        claimed_value: false,
+      }
+    );
+
+    // We get the error message based on the errors
+    let messageDetail = "Failed to transfer the tokens to the user wallet";
+
+    // We print everything so can be debug and maybe manually approach it
+    if (unctionFallbackError) {
+      const rollbackMessage = "Failed to rollback the claimed status";
+      console.error(rollbackMessage);
+      console.error("For buys IDs: ", JSON.stringify(buysIdToUpdate));
+      console.error("Token IDs: ", JSON.stringify(tokenIdsString));
+      console.error(unctionFallbackError);
+
+      messageDetail = `${messageDetail}. ${rollbackMessage}`;
+    }
+
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+        details: messageDetail,
+      },
+      { status: 500 }
+    );
+  }
+
+  // We add the transfer tx hash to the correct ids
+  const { error: updateTxError } = await createClient(true)
+    .from("stripe_buys")
+    .update({ claim_tx: transferResp.txHash })
+    .in("id", buysIdToUpdate);
+
+  // If there is an error on this update, we do not care since the
+  // sensitive part (transfering the tokens), was already made
+  if (updateTxError) {
+    console.error("Transfer tx hash was not added to token IDs:");
+    console.error(JSON.stringify(tokenIds.map((val_) => val_.toString())));
+    console.error(updateTxError);
+  }
+
   return NextResponse.json({
     message: "ok",
-    txHash: resp.txHash,
+    txHash: transferResp.txHash,
+    tokenIds: tokenIdsString,
   });
 }
