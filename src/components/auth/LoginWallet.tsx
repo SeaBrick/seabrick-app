b92820eb92378b11b63d4c876c262e942b39d078
NanezX <victor.nanezj@gmail.com>
2024-11-08 01:11:30 -0400

    remove comments

--- src/components/auth/LoginWallet.tsx
+++ src/components/auth/LoginWallet.tsx
@@ -11,17 +11,6 @@ import { loginWithWallet } from "@/app/login/actions";
 import { RpcRequestError, UserRejectedRequestError } from "viem";
 import Link from "next/link";
 
-// should be using modal
-// should open the modal and ask to connect wallet if not connected
-// should check if the address connected is registered or not
-//     - if registered, can login with wallet
-//     - if not registered, should not let the user login. Should show a button
-//          to redirect to register page
-// when click login, should ask to sign a message
-//     - if the user not sign the message, we show a error message on that
-//     - if the user sign, should made the request automatically
-// if the response is ok, this redirect to home page
-// if the response is negative, show message (most likely not happening but we don't know )
 interface LoginWallet {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
