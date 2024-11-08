3c7c234bd6aee53b28621481d8a33fb30ef63499
NanezX <victor.nanezj@gmail.com>
2024-11-07 21:26:23 -0400

    code reorganized

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,18 +1,11 @@
 import { type Chain, http } from "viem";
 import { arbitrum, arbitrumSepolia } from "wagmi/chains";
 
-// TODO: Use this to generate the chains and transports correctly.
-// `appChains` should just one length, like [arbitrumSepolia] or [arbitrum]
 // `appTransports` should have just one key. Like [arbitrumSepolia.id] or [arbitrum.id]
-// export const definedChain: "arbitrum" |"testnet" = process.env.NEXT_PUBLIC_CHAIN?.toLocaleLowerCase() ?? "testnet";
 
+// export const definedChain: "arbitrum" |"testnet" = process.env.NEXT_PUBLIC_CHAIN?.toLocaleLowerCase() ?? "testnet";
 // if the env is not "arbitrum" or "testnet", we default it to testnet
 const chainEnv = process.env.NEXT_PUBLIC_CHAIN?.toLowerCase();
-export const definedChain: "arbitrum" | "testnet" =
-  chainEnv === "arbitrum" || chainEnv === "testnet" ? chainEnv : "testnet";
-
-export const appChains: [Chain, ...Chain[]] =
-  definedChain == "arbitrum" ? [arbitrum] : [arbitrumSepolia];
 
 const arbitrumTransport = {
   [arbitrum.id]: http(
@@ -26,6 +19,13 @@ const arbitrumSepoliaTransport = {
       arbitrumSepolia.rpcUrls.default.http[0]
   ),
 };
+
+export const definedChain: "arbitrum" | "testnet" =
+  chainEnv === "arbitrum" || chainEnv === "testnet" ? chainEnv : "testnet";
+
+// `appChains` should just one length, like [arbitrumSepolia] or [arbitrum]
+export const appChains: [Chain, ...Chain[]] =
+  definedChain == "arbitrum" ? [arbitrum] : [arbitrumSepolia];
 
 // RPC URL for each chain
 // Example: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`
