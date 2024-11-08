08eb1e545114f0118aaba7bc6a4c28da0ab53485
NanezX <victor.nanezj@gmail.com>
2024-11-08 00:54:19 -0400

    removed unused components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { useAccount, useSignMessage } from "wagmi";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
-import ConnectButton from "@/components/buttons/ConnectButton";
-import Modal from "@/components/modals/Modal";
-import Container from "@/components/utils/Container";
-import { login, signinWithWallet, signUpWithWallet, signup } from "./actions";
+import { login } from "./actions";
 import { useAuth } from "@/context/authContext";
-import { createClient } from "@/lib/supabase/client";
-import SigninWalletModal from "@/components/modals/SigninWalletModal";
-import { useFormState } from "react-dom";
 import Image from "next/image";
 import Link from "next/link";
 import SubmitButton from "@/components/buttons/SubmitButton";
@@ -18,129 +11,6 @@ import LoginWallet from "@/components/auth/LoginWallet";
 import { Errors } from "@/lib/interfaces";
 
 // TODO: Add captchas
-function LoginWalletForm() {
-  const { address, isConnected } = useAccount();
-  const { signMessageAsync } = useSignMessage();
-  const { refetch } = useAuth();
-
-  const [needRegister, setNeedRegister] = useState<boolean>(false);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [formDataWallet, setFormDataWallet] = useState<FormData>();
-
-  // TODO: Fix issue that init message state does not reset.
-  // Need to use `useState` as I did on SigninWalletModal
-  const initMessageState = { message: "" };
-  const [messageState, formActionState] = useFormState(
-    formActionSignIn,
-    initMessageState
-  );
-
-  useEffect(() => {
-    async function isAddressAccount() {
-      const { error: userError } = await createClient()
-        .from("wallet_users")
-        .select("address")
-        .eq("address", address)
-        .single();
-
-      if (userError && userError.code == "PGRST116") {
-        setNeedRegister(true);
-      }
-
-      if (!userError) {
-        setNeedRegister(false);
-      }
-    }
-
-    if (isConnected) {
-      isAddressAccount();
-    }
-  }, [isConnected, address]);
-
-  async function formActionSignIn(
-    currentState: { message: string },
-    formData: FormData
-  ) {
-    try {
-      const address = formData.get("address")?.toString();
-      if (!address) {
-        return { message: "No address passed or connected to log in" };
-      }
-
-      const params = new URLSearchParams({ address });
-      const response = await fetch(`/api/request_message?${params}`, {
-        method: "GET",
-      });
-
-      if (response.ok) {
-        // If the response is OK, retrieve and sign the message
-        const messageGenerated = (await response.json()).message;
-
-        // Sign the message
-        const resp = await signMessageAsync({ message: messageGenerated });
-
-        // Store signed message in formData
-        formData.set("signature", resp);
-
-        if (needRegister) {
-          setIsOpen(true);
-          setFormDataWallet(formData);
-        } else {
-          await signinWithWallet(currentState, formData);
-          await refetch();
-        }
-
-        return { message: "" };
-      } else {
-        // Cannot get the message from server to sign in
-        return { message: "Error getting the message to sign" };
-      }
-    } catch (error) {
-      console.error(error);
-      return { message: "unknown error" };
-    }
-  }
-
-  return (
-    <>
-      {isOpen && (
-        <SigninWalletModal
-          open={isOpen}
-          setOpen={setIsOpen}
-          formAction={signUpWithWallet}
-          formData={formDataWallet}
-        />
-      )}
-      <div className="flex flex-col gap-y-4 items-center w-full max-w-xl">
-        <ConnectButton />
-
-        {isConnected && (
-          <form
-            className="flex flex-col gap-y-4 w-full"
-            action={formActionState}
-          >
-            <input
-              id="address"
-              name="address"
-              type="text"
-              hidden
-              readOnly
-              value={address}
-              className="disabled:cursor-not-allowed"
-            />
-            <button
-              className="bg-seabrick-green p-2 rounded-md text-white"
-              type="submit"
-            >
-              Sign in
-            </button>
-          </form>
-        )}
-      </div>
-    </>
-  );
-}
-
 export default function LoginPage() {
   const [haveWallet, setHaveWallet] = useState<boolean>(false);
   const [loginWallet, setLoginWallet] = useState<boolean>(false);
