b2b259c4ecd3031167942b5f7405fd558166ffa6
NanezX <victor.nanezj@gmail.com>
2024-11-07 22:45:03 -0400

    changes submit button

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -9,15 +9,9 @@ interface SubmitButtonProps {
   buttonClass?: string;
 }
 export default function SubmitButton(props: SubmitButtonProps) {
-  const {
-    label,
-    loadingLabel,
-    disable,
-    disabledTitle,
-    buttonClass = "grow shrink basis-0 self-stretch bg-seabrick-blue rounded-[5px] justify-center items-center gap-2.5 hover:bg-seabrick-blue/80",
-  } = props;
+  const { label, loadingLabel, disable, disabledTitle, buttonClass } = props;
   const sharedButtonClass =
-    "disabled:cursor-not-allowed disabled:bg-gray-400 text-center text-white text-sm font-normal font-['Noto Sans']";
+    "grow shrink basis-0 self-stretch bg-seabrick-blue rounded-[5px] justify-center items-center gap-2.5 hover:bg-seabrick-blue/80 disabled:cursor-not-allowed disabled:bg-gray-400 text-center text-white text-sm font-normal font-['Noto Sans']";
   const { pending } = useFormStatus();
 
   return (
