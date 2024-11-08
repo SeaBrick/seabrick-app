1331aebfb3b2651d080729e86c242f627fa302f4
NanezX <victor.nanezj@gmail.com>
2024-11-07 21:20:55 -0400

    chains config

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,12 +1,17 @@
-import { http } from "viem";
+import { type Chain, http } from "viem";
 import { arbitrumSepolia } from "wagmi/chains";
 
 // TODO: Use this to generate the chains and transports correctly.
 // `appChains` should just one length, like [arbitrumSepolia] or [arbitrum]
 // `appTransports` should have just one key. Like [arbitrumSepolia.id] or [arbitrum.id]
-export const definedChain = process.env.NEXT_PUBLIC_CHAIN ?? "testnet";
+// export const definedChain: "arbitrum" |"testnet" = process.env.NEXT_PUBLIC_CHAIN?.toLocaleLowerCase() ?? "testnet";
 
-export const appChains = [arbitrumSepolia];
+// if the env is not "arbitrum" or "testnet", we default it to testnet
+const chainEnv = process.env.NEXT_PUBLIC_CHAIN?.toLowerCase();
+export const definedChain: "arbitrum" | "testnet" =
+  chainEnv === "arbitrum" || chainEnv === "testnet" ? chainEnv : "testnet";
+
+export const appChains: [Chain, ...Chain[]] = [arbitrumSepolia];
 
 // RPC URL for each chain
 // Example: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`
@@ -17,5 +22,4 @@ export const appTransports = {
       arbitrumSepolia.rpcUrls.default.http[0]
   ),
 };
-
-
+    
