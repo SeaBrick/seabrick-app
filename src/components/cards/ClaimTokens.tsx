1d7f2909eb503d1b3d1d3b48b83136be872e7f92
Jose Prieto <jose.c.prieto.c2015@gmail.com>
2024-11-08 21:17:50 -0400

    added modal done to claim tokens

--- src/components/cards/ClaimTokens.tsx
+++ src/components/cards/ClaimTokens.tsx
@@ -1,7 +1,8 @@
 import { CurrencyDollarIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useState } from "react"
 import ClaimModal from "../modals/ClaimModal"
+import { ModalDone } from "../modals/ModalDone"
 interface TokensMap {
   id: number
   symbol: string
@@ -42,9 +43,6 @@ const testData: TokensMap[] = [
 ]
 const mapTokenData: TokensMap[] = testData
 
-function claimToken(token: TokensMap) {
-  console.log("I Claimed ", token.symbol)
-}
 export default function ClaimTokens({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   open,
@@ -54,38 +52,69 @@ export default function ClaimTokens({
   open: boolean
   setOpen: Dispatch<SetStateAction<boolean>>
 }) {
+  const [isSelfOpen, setSelfOpen] = useState(true)
+  const [isDoneOpen, setDoneOpen] = useState(false)
+  const [tokensClaimed, setTokensClaimed] = useState(0)
+  const [tokenSymbol, setTokensSymbol] = useState("")
+
+  function claimToken(token: TokensMap) {
+    setTokensClaimed(token.amount)
+    setTokensSymbol(token.symbol)
+    setSelfOpen(false)
+    setDoneOpen(true)
+    console.log("I Claimed ", token.symbol)
+  }
   return (
-    <ClaimModal open={open} setOpen={setOpen} title={"Tokens"}>
-      <div className="gap-2 flex flex-col overflow-y-auto">
-        {mapTokenData.map((t, i) => {
-          return (
-            <div className="flex gap-3 justify-between" key={i}>
-              <div className="px-3 py-1 grow shrink rounded-[5px] border border-[#babcc3]/60 justify-start items-center gap-2.5 inline-flex ">
-                <div className="rounded-[100px] bg-slate-200 h-[20px] w-[20px] flex justify-center items-center">
-                  <Image
-                    src={t.logo}
-                    alt={t.symbol}
-                    height={20}
-                    width={20}
-                    className="h-[20px] w-[20px] object-scale-down"
-                  />
+    <>
+      {isDoneOpen && (
+        <ModalDone
+          title="Sucessfully Claimed"
+          message={
+            <p>
+              You successfully claimed{" "}
+              <strong>
+                {tokensClaimed} {tokenSymbol}
+              </strong>{" "}
+              to your wallet
+            </p>
+          }
+          action={setOpen}
+        />
+      )}
+      {isSelfOpen && (
+        <ClaimModal open={open} setOpen={setOpen} title={"Tokens"}>
+          <div className="gap-2 flex flex-col overflow-y-auto">
+            {mapTokenData.map((t, i) => {
+              return (
+                <div className="flex gap-3 justify-between" key={i}>
+                  <div className="px-3 py-1 grow shrink rounded-[5px] border border-[#babcc3]/60 justify-start items-center gap-2.5 inline-flex ">
+                    <div className="rounded-[100px] bg-slate-200 h-[20px] w-[20px] flex justify-center items-center">
+                      <Image
+                        src={t.logo}
+                        alt={t.symbol}
+                        height={20}
+                        width={20}
+                        className="h-[20px] w-[20px] object-scale-down"
+                      />
+                    </div>
+                    <div className="text-sm">{t.amount}</div>
+                    <div className="text-sm font-bold">
+                      {t.symbol.toUpperCase()}
+                    </div>
+                  </div>
+                  <button
+                    className="bg-[#2069a0] hover:bg-[#17548b] active:bg-[#4290d6] rounded-[5px] p-3 flex text-white items-center justify-between text-sm"
+                    onClick={() => claimToken(t)}
+                  >
+                    <CurrencyDollarIcon className="size-[1.25rem] inline mr-1" />{" "}
+                    Claim
+                  </button>
                 </div>
-                <div className="text-sm">{t.amount}</div>
-                <div className="text-sm font-bold">
-                  {t.symbol.toUpperCase()}
-                </div>
-              </div>
-              <button
-                className="bg-[#2069a0] hover:bg-[#17548b] active:bg-[#4290d6] rounded-[5px] p-3 flex text-white items-center justify-between text-sm"
-                onClick={() => claimToken(t)}
-              >
-                <CurrencyDollarIcon className="size-[1.25rem] inline mr-1" />{" "}
-                Claim
-              </button>
-            </div>
-          )
-        })}
-      </div>
-    </ClaimModal>
+              )
+            })}
+          </div>
+        </ClaimModal>
+      )}
+    </>
   )
 }
