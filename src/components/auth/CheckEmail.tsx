722f1c93772629900d7aafbeaa98411b68608ba5
NanezX <victor.nanezj@gmail.com>
2024-11-09 12:39:02 -0400

    text prop

--- src/components/auth/CheckEmail.tsx
+++ src/components/auth/CheckEmail.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 interface CheckEmailProps {
   email: string;
+  text?: string;
 }
 
-const CheckEmail: React.FC<CheckEmailProps> = ({ email }) => {
+const CheckEmail: React.FC<CheckEmailProps> = ({ email, text }) => {
   return (
     <div className="w-[606px] h-[399px] p-6 bg-white rounded-[10px] flex-col justify-start items-center gap-8 inline-flex z-10 mt-[180px]">
       <div className="self-stretch h-[223px] flex-col justify-start items-center gap-4 flex">
@@ -15,17 +16,26 @@ const CheckEmail: React.FC<CheckEmailProps> = ({ email }) => {
             Check your email!
           </div>
         </div>
-        <div className="self-stretch text-[#8a8a8f] text-sm font-normal font-['Noto Sans']">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </div>
+        {text && (
+          <div className="self-stretch text-[#8a8a8f] text-sm font-normal font-['Noto Sans']">
+            {text}
+          </div>
+        )}
+
+        {/* TODO: Remove this. ONly for dev */}
+        {!text && (
+          <div className="self-stretch text-[#8a8a8f] text-sm font-normal font-['Noto Sans']">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industrys standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+        )}
       </div>
       <div className="self-stretch h-24 flex-col justify-start items-start gap-4 flex">
         <div className="self-stretch h-[19px] flex-col justify-center items-start gap-2 flex">
