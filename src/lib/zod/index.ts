82e49e38093da9720b1a8799e43c2e02481be78a
NanezX <victor.nanezj@gmail.com>
2024-11-09 11:34:46 -0400

    fixed register scema

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -2,11 +2,18 @@ import { isAddress, isHex } from "viem";
 import { z } from "zod";
 
 /**
- * Schema for Login and Register validation
+ * Schema for Login with email
  */
 export const UserAuthSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+/**
+ * Schema for Register with email
+ */
+export const UserAuthRegisterSchema = UserAuthSchema.extend({
+  fullName: z.string().min(1, "Name is required"),
 });
 
 export const userLoginWalletSchema = z.object({
