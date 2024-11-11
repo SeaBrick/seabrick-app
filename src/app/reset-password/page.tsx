d7f34b8283422da4d7fa388ac3e402e050c04ab9
NanezX <victor.nanezj@gmail.com>
2024-11-11 04:24:43 -0400

    linter

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -3,18 +3,15 @@ import React from "react";
 import ResetPasswordForm from "@/components/forms/ResetPassword";
 import { LoginBanner } from "@/components/layout/LoginBanner";
 
-
-
-
 export default function ResetPasswordPage() {
-  return(
+  return (
     <>
-    <LoginBanner/>
-    <div className="flex justify-center">
-        <div className="flex flex-col z-10">          
-          <ResetPasswordForm/>  
+      <LoginBanner />
+      <div className="flex justify-center">
+        <div className="flex flex-col z-10">
+          <ResetPasswordForm />
         </div>
-    </div>
+      </div>
     </>
-    );
+  );
 }
