30323231b6683a98ba2e654fd98d13204c3ec057
NanezX <victor.nanezj@gmail.com>
2024-11-10 23:25:49 -0400

    fixing errors at links

--- src/components/dropdowns/AccountDropdown.tsx
+++ src/components/dropdowns/AccountDropdown.tsx
@@ -21,7 +21,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronDownIcon } from '@heroicons/react/16/solid'
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 interface AccountDropdownProps {
   num?: number;
@@ -36,9 +36,11 @@ export default function AccountDropdown({ num: _num }: AccountDropdownProps) {
   return (
     <Menu>
       <div className="flex flex-col items-end gap-1">
-        <h3 className="text-base text-dark-blue font-semibold">Sebastian Rojas</h3>
+        <h3 className="text-base text-dark-blue font-semibold">
+          Sebastian Rojas
+        </h3>
         <MenuButton className="flex text-sm items-center text-light-gray gap-1 rounded-md data-[focus]:outline-1 data-[focus]:outline-white">
-          My Account         
+          My Account
           <ChevronDownIcon className="size-4 fill-light-gray" />
         </MenuButton>
 
@@ -47,32 +49,51 @@ export default function AccountDropdown({ num: _num }: AccountDropdownProps) {
           anchor="bottom end"
           className="w-46 origin-top-right rounded-xl border bg-white p-1 z-50 text-right transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
         >
-          <MenuSection>           
+          <MenuSection>
             <MenuItem>
-              <button className="group flex w-full text-dark-blue font-['Montserrat'] text-sm items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10"> 
-                <Link href="/account">Account Settings</Link>
+              <Link
+                href="/account"
+                prefetch={true}
+                className="group flex w-full items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10"
+              >
+                <span className="text-dark-blue font-['Montserrat'] text-sm">
+                  Account Settings
+                </span>
                 <Cog6ToothIcon className="size-6 text-dark-blue" />
-              </button>
+              </Link>
+              {/* </button> */}
             </MenuItem>
             <MenuSeparator className="my-1 h-px bg-light-gray/10" />
             <MenuItem>
-              <button className="group flex w-full text-dark-blue font-['Montserrat'] text-sm items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10">               
-                <Link href="/account?tab=transactions">Transactions</Link>
+              <Link
+                href="/account?tab=transactions"
+                prefetch={true}
+                className="group flex w-full items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10"
+              >
+                <span className="text-dark-blue font-['Montserrat'] text-sm ">
+                  Transactions
+                </span>
                 <ArrowsUpDownIcon className="size-6 text-dark-blue" />
-              </button>
+              </Link>
             </MenuItem>
             {(user && userRole == "admin") ||
-            (userRole == "owner" && (
-              <>
-                <MenuSeparator className="my-1 h-px bg-light-gray/10" />
-                <MenuItem>
-                  <button className="group flex w-full text-dark-blue font-['Montserrat'] text-sm items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10">               
-                    <Link href="/admin-list">Admin</Link>
-                    <UserCircleIcon className="size-6 text-dark-blue" />
-                  </button>
-                </MenuItem>
-              </>
-            ))}
+              (userRole == "owner" && (
+                <>
+                  <MenuSeparator className="my-1 h-px bg-light-gray/10" />
+                  <MenuItem>
+                    <Link
+                      href="/admin-list"
+                      prefetch={true}
+                      className="group flex w-full items-center justify-end gap-2 rounded-lg py-1.5 px-4 data-[focus]:bg-black/10"
+                    >
+                      <span className="text-dark-blue font-['Montserrat'] text-sm ">
+                        Admin
+                      </span>
+                      <UserCircleIcon className="size-6 text-dark-blue" />
+                    </Link>
+                  </MenuItem>
+                </>
+              ))}
           </MenuSection>
           <MenuSeparator className="my-1 h-px bg-light-gray/10" />
           <MenuItem>
@@ -83,7 +104,7 @@ export default function AccountDropdown({ num: _num }: AccountDropdownProps) {
               {userType == "wallet" ? <>Disconnect</> : <>Log Out</>}
               <ArrowLeftStartOnRectangleIcon className="size-6 text-dark-blue" />
             </button>
-          </MenuItem>         
+          </MenuItem>
         </MenuItems>
       </div>
       <Image
