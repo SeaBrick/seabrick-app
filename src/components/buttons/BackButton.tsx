b606f94ffac8a9c09905a7383d7f5b718ad570da
Jose Prieto <jose.c.prieto.c2015@gmail.com>
2024-11-11 13:55:31 -0400

    fixed bad behaved button

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,12 +1,16 @@
-import {  ArrowLeftIcon } from '@heroicons/react/24/outline'
-import { useRouter } from 'next/navigation' 
+import { ArrowLeftIcon } from "@heroicons/react/24/outline"
+import { useRouter } from "next/navigation"
 
 export default function BackButton() {
   const router = useRouter()
 
   return (
-    <button className='group flex items-center gap-1 text-light-gray text-normal hover:text-text-gray/90 capitalize' type="button" onClick={() => router.back()}>
-      <ArrowLeftIcon className='w-5 h-5 group-hover:-translate-x-1 duration-300' />
+    <button
+      className="group flex items-center gap-1 text-light-gray text-normal hover:text-text-gray/90 capitalize z-10 relative"
+      type="button"
+      onClick={() => router.back()}
+    >
+      <ArrowLeftIcon className="w-5 h-5 group-hover:-translate-x-1 duration-300" />
       Go back
     </button>
   )
