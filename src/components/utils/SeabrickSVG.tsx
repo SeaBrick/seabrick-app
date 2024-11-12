96723c85b0a00a6a7d3278d9b1d3c3cfdfa7a829
NanezX <victor.nanezj@gmail.com>
2024-11-12 19:25:15 -0400

    use correct import

--- src/components/utils/SeabrickSVG.tsx
+++ src/components/utils/SeabrickSVG.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 function SeabrickSVG() {
   return (
     <Link href="/dashboard" prefetch={true}>
-      <Image src="./seabrick.svg" width={293.02} height={29} alt="Seabrick" />
+      <Image src="/seabrick.svg" width={293.02} height={29} alt="Seabrick" />
     </Link>
   );
 }
