d989c760441864d3edeb38637f577ddb712a5373
NanezX <victor.nanezj@gmail.com>
2024-11-13 21:04:10 -0400

    wip buy page

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,15 +1,23 @@
 "use client";
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 // import RequireWallet from "@/app/components/utils/RequireWallet";
 import BuyNFT from "./BuyNFT";
 import PageLoaderSpinner from "@/components/spinners/PageLoaderSpinner";
 import { useAuth } from "@/context/authContext";
 import { useRouter } from "next/navigation";
 import BuyWithStripe from "./BuyWithStripe";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/react";
+import { AtSymbolIcon } from "@heroicons/react/24/outline";
+
+enum TabsIndex {
+  STRIPE,
+  CRYPTO,
+}
 
 export default function BuyPage() {
   const { user } = useAuth();
   const router = useRouter();
+  const [selectedIndex, setSelectedIndex] = useState<number>(TabsIndex.STRIPE);
 
   useEffect(() => {
     if (!user) {
@@ -20,9 +28,53 @@ export default function BuyPage() {
 
   return (
     <div className="w-1/2 mx-auto">
-      <p className="text-3xl font-bold mb-8">Buy Seabrick NFT</p>
+      <p className="text-black text-4xl font-normal font-['Noto Sans'] text-center my-8">
+        Buy Seabrick
+      </p>
 
-      {user && (
+      <TabGroup
+        selectedIndex={selectedIndex}
+        onChange={setSelectedIndex}
+        className="z-10 relative"
+      >
+        <TabList className="flex gap-4 px-5">
+          <Tab
+            className={`rounded-t-xl py-2 px-3 outline-none ${selectedIndex == TabsIndex.STRIPE ? "bg-white" : "bg-gray-200 hover:bg-gray-300 shadow-inner"}`}
+          >
+            <button className="flex items-center gap-x-1">
+              <AtSymbolIcon className="size-4" />
+              <div
+                className={`${selectedIndex == TabsIndex.STRIPE ? "text-[#49414d]" : "text-[#8a8a8f]"} text-xl font-bold font-['Noto Sans'] leading-normal`}
+              >
+                Credit/Debit Card
+              </div>
+            </button>
+          </Tab>
+          <Tab
+            className={`rounded-t-xl py-2 px-3 outline-none ${selectedIndex == TabsIndex.CRYPTO ? "bg-white" : "bg-gray-200 hover:bg-gray-300 shadow-inner"}`}
+          >
+            <button className="flex items-center gap-x-1">
+              <AtSymbolIcon className="size-4" />
+              <div
+                className={`${selectedIndex == TabsIndex.CRYPTO ? "text-[#49414d]" : "text-[#8a8a8f]"} text-xl font-bold font-['Noto Sans'] leading-normal`}
+              >
+                Crypto
+              </div>
+            </button>
+          </Tab>
+        </TabList>
+        <TabPanels className="px-5">
+          <TabPanel className="-5">
+            <BuyWithStripe />
+          </TabPanel>
+          <TabPanel className="-5">
+            <p>CRYPTOOOOOOOOO</p>
+            {/* <RegisterWalletForm /> */}
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+
+      {/* {user && (
         <>
           {user.user_metadata.type == "email" && <BuyWithStripe />}
 
@@ -38,7 +90,7 @@ export default function BuyPage() {
             </Suspense>
           )}
         </>
-      )}
+      )} */}
     </div>
   );
 }
