0f99c5f26ba25d7aceb61d7bb0b06a9b023264f4
NanezX <victor.nanezj@gmail.com>
2024-11-14 11:58:16 -0400

    separate component

--- src/components/cards/BuyNFTCrypto.tsx
+++ src/components/cards/BuyNFTCrypto.tsx
@@ -4,80 +4,18 @@ import SubmitButton from "../buttons/SubmitButton";
 import { useContractContext } from "@/context/contractContext";
 import AggregatorsLoader from "../loaders/AggregatorsLoader";
 import { Aggregator, ERC20Token } from "@/lib/interfaces";
-import ImageFallback from "../images/ImageFallback";
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import {
   aggregatorV3InterfaceAbi,
   ierc20Abi,
   iMarketAbi,
 } from "@/lib/contracts/abis";
-import { Address, formatUnits } from "viem";
+import { formatUnits } from "viem";
 import ConnectButton from "../buttons/ConnectButton";
 import Link from "next/link";
 import ApproveERC20Modal from "../modals/ApproveERC20Modal";
 import { toast } from "react-toastify";
-
-interface SelecTokenButtonProps {
-  setSelect: () => void;
-  isSelected: boolean;
-  token: ERC20Token;
-  aggregator: Aggregator;
-  walletAddress: Address;
-}
-const SelecTokenButton: React.FC<SelecTokenButtonProps> = ({
-  token,
-  setSelect,
-  isSelected,
-  walletAddress,
-}) => {
-  const { data: balance } = useReadContract({
-    abi: ierc20Abi,
-    address: token.address,
-    functionName: "balanceOf",
-    args: [walletAddress],
-  });
-
-  return (
-    <div className="grow shrink basis-0 justify-end items-end gap-4 flex">
-      <div className="grow shrink basis-0 self-stretch flex-col justify-center items-start gap-2 inline-flex">
-        <button
-          onClick={setSelect}
-          type="button"
-          className={`self-stretch grow shrink basis-0 px-[15px] py-2.5 rounded-[5px] border border-[#babcc3]/60 justify-start items-center gap-2.5 inline-flex hover:bg-[#2069a0] hover:text-white ${isSelected && "bg-[#2069a0] text-white"}`}
-        >
-          <div className="w-[25px] h-[25px] relative">
-            <div className="w-[22.22px] left-[1.40px] top-[1.39px] absolute">
-              <ImageFallback
-                width={25}
-                height={25}
-                className="w-[25px] h-[25px] object-cover"
-                //  src="/tokens/ETH-R.webp"
-                src={`/tokens/${token.symbol}.webp`}
-                fallbackSrc="/tokens/empty-token.svg"
-                alt="Token"
-              />
-            </div>
-          </div>
-          <div className="text-current text-sm font-normal font-['Noto Sans'] flex gap-x-2">
-            {isSelected && (
-              <span>
-                {parseFloat(
-                  formatUnits(
-                    BigInt(balance?.toString() || "0"),
-                    parseInt(token.decimals)
-                  )
-                ).toFixed(2)}
-              </span>
-            )}
-            <span className={`${isSelected && "font-bold"}`}>
-              {token.symbol}
-            </span>
-          </div>
-        </button>
-      </div>
-    </div>
-  );
-};
+import SelecTokenButton from "../buttons/SelectTokenButton";
 
 // TODO: Send transaction as async, wait for the tx hash. Then use the tx hash and get the logs
 // to see if the tokens were buyed
