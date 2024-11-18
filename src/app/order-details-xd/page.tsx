dcb22a6098c0e4e084d570c5bc5cb1116a539209
NanezX <victor.nanezj@gmail.com>
2024-11-18 05:13:18 -0400

    added valid info

--- src/app/order-details-xd/page.tsx
+++ src/app/order-details-xd/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { HomeIcon } from "@heroicons/react/24/outline";
 import DetailsTable, { type DetailsTableProps } from "./DetailsTable";
-import { isHash } from "viem";
+import { type Hex, isHash } from "viem";
 import { getBuysByTransaction } from "@/lib/subgraph";
-import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
+import { getUrl } from "@/lib/utils";
+import { headers } from "next/headers";
 
 interface OrderDetailsProps {
   searchParams: {
@@ -22,60 +24,124 @@ export default async function OrderDetails({
   // And alse we define the const/object for the table colums
   let tableData: DetailsTableProps;
 
-  try {
-    if (type === "crypto") {
-      if (!hash || !isHash(hash)) {
-        throw new Error("Invalid hash for Crypto transaction");
-      }
-      // Perform for crypto
-      const resp = await getBuysByTransaction(hash);
-
-      // No response data
-      if (resp === null || resp.length === 0) {
-        throw new Error("No data found for this transaction hash");
-      }
-
-      // Total amount (plus all the Buys at the same tx hash)
-      let totalAmountPaid = 0n;
-      const tokensId = resp.map((buy_) => {
-        // Increasing to know the total amount paid
-        totalAmountPaid = totalAmountPaid + BigInt(buy_.amountPaid);
-
-        return buy_.tokenId;
-      });
-
-      tableData = {
-        type,
-        txHash: hash,
-        paymentToken: resp[0].paymentToken,
-        totalAmountPaid,
-        tokensId,
-        blockNumber: resp[0].blockNumber,
-        blockTimestamp: resp[0].blockTimestamp,
-        buysData: resp,
-      };
+  // ALL the error are catched by the Error.tsx boundary
+  if (type === "crypto") {
+    if (!hash || !isHash(hash)) {
+      throw new Error("Invalid hash for Crypto transaction");
     }
-    //
-    else if (type === "stripe") {
-      if (!session_id) {
-        throw new Error("Invalid ID Stripe transaction");
-      }
+    // Perform for crypto
+    const resp = await getBuysByTransaction(hash);
 
-      tableData = {
-        type,
-        tokensId: [""],
-      };
-
-      // Perform for stripe
-    }
-    //
-    else {
-      throw new Error("Invalid transaction");
+    // No response data
+    if (resp === null || resp.length === 0) {
+      throw new Error("No data found for this transaction hash");
     }
 
-    //
-  } catch (error) {
-    throw error;
+    // Total amount (plus all the Buys at the same tx hash)
+    let totalAmountPaid = 0n;
+    const tokensId = resp.map((buy_) => {
+      // Increasing to know the total amount paid
+      totalAmountPaid = totalAmountPaid + BigInt(buy_.amountPaid);
+
+      return buy_.tokenId;
+    });
+
+    tableData = {
+      type,
+      txHash: hash,
+      paymentToken: resp[0].paymentToken,
+      totalAmountPaid,
+      tokensId,
+      blockNumber: resp[0].blockNumber,
+      blockTimestamp: resp[0].blockTimestamp,
+      buysData: resp,
+    };
+  }
+  //
+  else if (type === "stripe") {
+    if (!session_id) {
+      throw new Error("Invalid ID Stripe transaction");
+    }
+
+    const supabaseClient = createClient();
+
+    let { data: sessionData, error: sessionError } = await supabaseClient
+      .from("stripe_checkout_sessions")
+      .select("id, fulfilled, created_at")
+      .eq("session_id:", session_id)
+      .single<{ id: string; fulfilled: boolean; created_at: string }>();
+
+    if (sessionError) {
+      throw new Error(`Sessions: ${sessionError.message}`);
+    }
+
+    if (!sessionData) {
+      throw new Error("ID Stripe transaction not found");
+    }
+
+    if (!sessionData.fulfilled) {
+      // Not fulfiled, need to wait
+      throw new Error("Stripe transaction not processed. Need to wait");
+    }
+
+    let { data: buysData, error: buysError } = await supabaseClient
+      .from("stripe_buys")
+      .select("token_id, claimed, tx_hash, created_at")
+      .eq("session_id", sessionData.id)
+      .returns<
+        {
+          token_id: string;
+          claimed: boolean;
+          tx_hash: Hex;
+          created_at: string;
+        }[]
+      >();
+
+    if (buysError) {
+      throw new Error(`Buys: ${buysError.message}`);
+    }
+
+    if (!buysData || buysData.length === 0) {
+      throw new Error("No buys found for this transaction");
+    }
+
+    const appUrl = getUrl(headers().get("referer"));
+
+    const response = await fetch(
+      `${appUrl}/api/stripe/checkout_sessions?session_id=${session_id}`,
+      {
+        method: "GET",
+        headers: headers(),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Stripe checkout session not found");
+    }
+
+    const stripeSessionData = (await response.json()) as {
+      customer_email: string;
+      totalAmount: number;
+      currency: string;
+    };
+
+    const tokensId = buysData.map((buys_) => buys_.token_id);
+
+    tableData = {
+      type,
+      stripeId: session_id,
+      tokensId,
+      totalAmount: stripeSessionData.totalAmount,
+      currency: stripeSessionData.currency,
+      buyerEmail: stripeSessionData.customer_email,
+      txHash: buysData[0].tx_hash,
+      createdAt: buysData[0].created_at,
+    };
+  }
+  // No valid trasanction
+  else {
+    // ALL the errors are catched by the error.tsx boundary
+    throw new Error("Invalid transaction");
   }
 
   return (
