15f36787d6bf012d2e1bb4d00a1e7a551874ac56
Jose Prieto <jose.c.prieto.c2015@gmail.com>
2024-11-19 23:22:51 -0400

    wip: needs connection

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: config => {
-      config.externals.push('pino-pretty', 'lokijs', 'encoding')
-      return config
-    },
-    redirects: async () => {
-      return []
-    }
-  }
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding")
+    config.resolve.fallback = { fs: false }
+    return config
+  },
+  redirects: async () => {
+    return []
+  },
+}
 
 module.exports = nextConfig
