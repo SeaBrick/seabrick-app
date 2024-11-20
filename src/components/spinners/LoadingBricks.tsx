c1b03dd1a3a1d3e0051ad17083b07c552e23445d
Rightles <ricardodiaz1999@gmail.com>
2024-11-20 14:18:10 -0400

    adding  claim token loader, empty state & fixing modal size

---
+++ src/components/spinners/LoadingBricks.tsx
@@ -0,0 +1,34 @@
+const LoadingBricks: React.FC = () => {
+    return (
+      <section className="bricks-container">    
+        <div className="boxes">
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </section>     
+    );
+};
+
+export default LoadingBricks;
