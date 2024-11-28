099d0fd27f4f7f537fa0fcdf94cc022b76098107
Jose Prieto <jose.c.prieto.c2015@gmail.com>
2024-11-28 01:38:32 -0400

    mail done

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,31 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 
 import * as ejs from "ejs"
 export default function RenderTest() {
   const [activeEditor, setActiveEditor] = useState(false)
+  const [activateInstructions, setActivateInstructions] = useState(false)
   const [template, setTemplate] = useState("")
   // get template for useContext
   // and set it automatically
-  const [render, setRender] = useState("<p>Your code Here</p>")
+  const [render, setRender] = useState("<p>Your result Here</p>")
 
   const router = useRouter()
+
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (event.altKey && event.key === "r") {
+        const renderButton = document.getElementById("render-button")
+        renderButton?.click()
+      }
+    }
+    document.addEventListener("keydown", listener)
+    return () => {
+      document.removeEventListener("keydown", listener)
+    }
+  }, [])
 
   function captureText(formData: FormData) {
     const raw = formData.get("raw") as string
@@ -55,12 +69,18 @@ export default function RenderTest() {
           </div>
           <div className="w-full flex justify-end gap-2">
             {activeEditor ? (
-              <button
-                className="px-4 py-2 bg-red-600 text-white text-base rounded-[5px] hover:cursor-pointer hover:bg-red-700 active:bg-red-500"
-                type="submit"
-              >
-                Render
-              </button>
+              <>
+                <span className="self-center mr-2 text-gray-800">
+                  Press Alt+R to Render
+                </span>
+                <button
+                  id="render-button"
+                  className="px-4 py-2 bg-red-600 text-white text-base rounded-[5px] hover:cursor-pointer hover:bg-red-700 active:bg-red-500"
+                  type="submit"
+                >
+                  Render
+                </button>
+              </>
             ) : (
               ""
             )}
@@ -71,37 +91,93 @@ export default function RenderTest() {
             >
               {activeEditor ? "Close Editor" : "Open Editor"}
             </button>
+            <button
+              className="px-4 py-2 bg-amber-500 text-white text-base rounded-[5px] hover:cursor-pointer hover:bg-amber-600 active:bg-amber-400"
+              type="button"
+              onClick={() => setActivateInstructions(!activateInstructions)}
+            >
+              {activateInstructions
+                ? "Close Instructions"
+                : "Open Instructions"}
+            </button>
           </div>
         </div>
 
         <div id="render-area" className="w-full h-full flex gap-4">
+          <div
+            id="instructions-area"
+            className={`w-full flex-col rounded-[10px] bg-sky-200	text-sky-900 p-4 ${activateInstructions ? "block" : "hidden"}`}
+          >
+            <p>
+              This is an HTML Editor, please write everything in plain HTML.
+            </p>
+            <p>To add variables, to it in the format:</p>
+            <code className="bg-sky-300 p-[1px] rounded-[5px]">{`<%={variable}%>`}</code>
+            <p>Example:</p>
+            <code className="bg-sky-300 p-[1px] rounded-[5px]">{`<p>Hello <%=user%>!</p>`}</code>
+            <p>
+              Expected Output:{" "}
+              <code className="bg-sky-300 p-[1px] rounded-[5px]">
+                Hello John!
+              </code>{" "}
+            </p>
+            <p> Available Variables:</p>
+            <p>
+              {" "}
+              - <code className="bg-sky-300 p-[1px]rounded-[5px]">
+                user:
+              </code>{" "}
+              The name of the token buyer{" "}
+            </p>
+            <p>
+              {" "}
+              - <code className="bg-sky-300 p-[1px] rounded-[5px]">
+                date:
+              </code>{" "}
+              {`Today's`} date{" "}
+            </p>
+            <p>
+              {" "}
+              -{" "}
+              <code className="bg-sky-300 p-[1px] rounded-[5px]">
+                token_id:
+              </code>{" "}
+              {`Token's`} identifier{" "}
+            </p>
+            <p>
+              {" "}
+              -{" "}
+              <code className="bg-sky-300 p-[1px] rounded-[5px]">
+                address:
+              </code>{" "}
+              Destination Address{" "}
+            </p>
+            <p>
+              {" "}
+              - <code className="bg-sky-300 p-[1px] rounded-[5px]">
+                hash:
+              </code>{" "}
+              Transaction Hash{" "}
+            </p>
+            <p>
+              -{" "}
+              <code className="bg-sky-300 p-[1px] rounded-[5px]">img_url:</code>{" "}
+              Seabrick logo url
+            </p>
+            <p>Press the {`"Render"`} button to visualize your code</p>
+          </div>
+
           <div
             id="editor-area"
             className={`w-full flex-col rounded-[10px] ${activeEditor ? "flex" : "hidden"}`}
           >
             <div id="input-area" className="flex w-full h-full">
               <textarea
-                className="bg-white w-full h-full p-3 rounded-[10px]"
+                className="bg-white text-red-700 border-sky-700 border-2 w-full h-full p-3 rounded-[10px]"
                 name="raw"
-                placeholder={`This is an HTML Editor, please write everything in plain HTML.
-    To add variables, to it in the format: 
-                    <%={variable}%>
-                  Example:
-                    <p>Hello <%=user%>!</p>
-                  Expected Output: 
-                    Hello John!  
-                  Available Variables:
-                      - user: The name of the token buyer
-                      - date: Today's date
-                      - token_id: Token's identifier
-                      - address: Destination Address
-                      - hash: Transaction Hash'
-                      - img_url: Seabrick logo url
-Press the "Render" button to visualize your code
-                  `}
+                placeholder={`Your code here`}
               ></textarea>
             </div>
-            <div id="error-area"></div>
           </div>
 
           <div
@@ -117,21 +193,4 @@ Press the "Render" button to visualize your code
       </form>
     </>
   )
-}
-// show actual
-// open editor
-// save? cancel?
-// editor: text part / instructions part
-// log error part
-
-{
-  /* <div>
-<textarea
-  name=""
-  id=""
-  onChange={captureText}
-  className="bg-white"
-></textarea>
-<div id="render"></div>
-</div> */
 }
