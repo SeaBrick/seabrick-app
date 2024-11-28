a32782a8c87e1552037d2a735471da1a3eceb82d
NanezX <victor.nanezj@gmail.com>
2024-11-28 19:16:09 -0400

    wip: just to show

--- src/app/api/resend/route.ts
+++ src/app/api/resend/route.ts
@@ -1,35 +1,17 @@
-// import { type NextRequest, NextResponse } from "next/server";
-// import { Resend } from "resend";
+import { sendReceipt } from "@/lib/email/receipts";
+import { NextResponse } from "next/server";
+import { zeroHash } from "viem";
 
-// const resend = new Resend("re");
-
-// export async function GET(request: NextRequest) {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: "Seabrick <onboarding@resend.dev>",
-//       to: ["victor.nanezj@gmail.com"],
-//       subject: "Hello man! Works! This is your receipt",
-//       html: "<strong>It works! You got your receipt!</strong>",
-//     });
-
-//     if (error) {
-//       console.error(error);
-//       return NextResponse.json({ error }, { status: 500 });
-//     }
-
-//     console.log("data: ");
-//     console.log(data);
-
-//     return NextResponse.json({ data }, { status: 200 });
-//     //
-//   } catch (e) {
-//     console.log("error:  ", e);
-
-//     let errorMsg = "Error no conocido";
-//     if (e instanceof Error) {
-//       errorMsg = e.message;
-//     }
-
-//     return NextResponse.json({ error: errorMsg }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  try {
+    const isSend = await sendReceipt(
+      "victor.nanezj@gmail.com",
+      ["20", "21", "22"],
+      zeroHash,
+      new Date()
+    );
+    return NextResponse.json({ isSend }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ isSend: false, error }, { status: 500 });
+  }
+}
